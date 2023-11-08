65124186323e6f625492abf62f98c6d53f6f1008
Jonathan Luciu <115652432+TheActualZenaldV2@users.noreply.github.com>
2023-11-08 08:55:04 -0800

    Create test.js

---
+++ assignments/daniel/test.js
@@ -0,0 +1,27 @@
+
+  let pyodide;
+  const pythonCodeTextarea = document.getElementById("pythonCode");
+  const outputDiv = document.getElementById("outputDiv");
+
+  async function initializePyodide() {
+    pyodide = await loadPyodide();
+    outputDiv.innerHTML = "Ready to run Python code.";
+  }
+
+  function runPythonCode() {
+    const myPython = pythonCodeTextarea.value;
+    outputDiv.innerHTML = "Running Python code...";
+
+    if (pyodide) {
+      try {
+        const result = pyodide.runPython(myPython);
+        outputDiv.innerHTML = "Output:<br>" + result;
+      } catch (err) {
+        outputDiv.innerHTML = "Error:<br>" + err.replace(/\n/g, '<br>');
+      }
+    } else {
+      outputDiv.innerHTML = "Pyodide is not initialized yet. Please wait.";
+    }
+  }
+
+  initializePyodide();
